1000414f90daf8465e571614a76bfb0f757ed357
Y Sussman <ypaulsussman@gmail.com>
2024-04-11 15:01:13 -0500

    add tailwind typography

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,7 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
 
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.amber.50);
-  }
-</style>
+<!-- <style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.amber.50);
+	}
+</style> -->
